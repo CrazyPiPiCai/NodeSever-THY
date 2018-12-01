890661e1c0f6957d0d8f4f216cc172180c16e13c
PiPi <1303798456@qq.com>
2018-12-02 01:03:32 +0800

    Update router.js
    测试服务器

--- router.js
+++ router.js
@@ -1,16 +1,20 @@
-﻿const express = require('express')
-const DB = require('./db')
-const router = express.Router()
+﻿const express = require("express");
+//const DB = require('./db')
+const router = express.Router();
 
-router.get('/all', (req, res) => {
+router.get("/all", (req, res) => {
+  /*
     var result = DB.start();
     result.then(val =>
         res.type('json').send(val)
     ).catch(err =>
         res.send(err)
     )
-})
+    */
+  res.send("测试OK");
+});
 
+/*
 router.get('/select', (req, res) => {
     const sheet_name = req.query.sheet_name;
     var result = DB.App(sheet_name);
@@ -20,5 +24,6 @@ router.get('/select', (req, res) => {
         res.send(err)
     )
 })
+*/
 
-module.exports = router
+module.exports = router;
